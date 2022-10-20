8e7e362c48d09b9f1a8564dbcc3b8b0bce4a8a38
Bohdan <b.i.dudchenko@student.khai.edu>
2022-10-20 17:30:00 +0300

    Resolved conflict
    Test.java added to feature_branch with change on string #2

--- src/main/java/org/example/Prac3/Test.java
+++ src/main/java/org/example/Prac3/Test.java
@@ -2,6 +2,7 @@ package org.example.Prac3;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Java");
+        System.out.println("Java education");
+
     }
 }
